bf26b7856e29eb81b04787e1b1e9415f7a94f1af
Роман З <geralt1530@gmail.com>
2020-07-04 18:56:56 +0300

    Add roles(need improvements)

---
+++ Baze/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Baze.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message); 
+        }
+    }
+}
---
+++ Baze/Models/ManageUsersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Baze.Models;
+
+namespace Baze.Models
+{
+    public class ManageUsersViewModel
+    {
+        public ApplicationUser[] Administrators { get; set; }
+
+        public ApplicationUser[] Everyone { get; set; } 
+    }
+}
