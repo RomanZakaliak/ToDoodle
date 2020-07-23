dc9d964ec27000d3e3823738ee771188faef49c2
Роман З <geralt1530@gmail.com>
2020-07-23 23:33:13 +0300

    Added new user entering chat notification

--- Baze/Hubs/ChatHub.cs
+++ Baze/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Todo.Hubs
 {
@@ -9,6 +10,12 @@ namespace Todo.Hubs
         public async Task Send(string message)
         {
             await this.Clients.All.SendAsync("Send", message); 
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", "New user entered chat!");
+            await base.OnConnectedAsync();
         }
     }
 }
